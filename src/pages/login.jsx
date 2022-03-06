eee758f2498ce42750d0b107b64352e82ba91149
Sipeng He <hsp8412@hotmail.com>
2022-03-06 12:10:54 -0700

    binding the login form with state

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -2,14 +2,23 @@ import React, { Component } from "react";
 import { Form, Button, Container, Row, Col } from "react-bootstrap";
 
 class LoginForm extends Component {
-  state = {};
+  state = {
+    account: { username: "", password: "" },
+  };
 
   handleSubmit = (e) => {
     e.preventDefault();
     console.log("Submitted");
   };
 
+  handleChange = ({ currentTarget }) => {
+    const account = { ...this.state.account };
+    account[currentTarget.name] = currentTarget.value;
+    this.setState({ account });
+  };
+
   render() {
+    const { username, password } = this.state.account;
     return (
       <Container>
         <Row>
@@ -19,15 +28,24 @@ class LoginForm extends Component {
               <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
                 <Form.Control
+                  name="username"
                   type="email"
                   placeholder="Enter email"
+                  value={username}
                   autoFocus
+                  onChange={this.handleChange}
                 />
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control
+                  name="password"
+                  type="password"
+                  placeholder="Password"
+                  onChange={this.handleChange}
+                  value={password}
+                />
               </Form.Group>
 
               <Button variant="primary" type="submit">
